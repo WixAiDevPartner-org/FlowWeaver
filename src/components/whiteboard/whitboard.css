5ff2bb47e5b387963774244f0f9f4cd951161f44
WixPartner <202474862+WixPartner@users.noreply.github.com>
2025-05-14 18:50:18 -0700

    Create whitboard.css
    @WixPartner

---
+++ src/components/whiteboard/whitboard.css
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Whiteboard.css';
+import Toolbar from './Toolbar';
+import Canvas from './Canvas';
+
+const Whiteboard = () => {
+  return (
+    <div className="whiteboard">
+      <Toolbar />
+      <Canvas />
+    </div>
+  );
+};
+
+export default Whiteboard;
